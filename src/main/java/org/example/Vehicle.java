37692853377f2b93783a94e75995d1d80f8edb2a
Denis Sirghi <denissirghi1999@gmail.com>
2024-09-15 02:13:32 +0300

    Inheritance

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -9,4 +9,18 @@ public class Vehicle {
     double maxSpeed;
     boolean  damaged;
     String color;
+
+    public double accelerate(double speed, double durationInHours) {
+
+      System.out.println(name+ "Is accelerating with" + speed +"Km/h" + durationInHours + "h");
+      double distance = speed * durationInHours;
+      totalTravelDistance= totalTravelDistance + distance;
+      System.out.println("Total Travel Distance for vehicle " + name + ":" + totalTravelDistance);
+
+      double usedFuel= distance * mileage/100;
+      fuelLevel -= usedFuel;
+      System.out.println("Remaining fuel for vehicle " + name + ":" + fuelLevel);
+
+      return distance;
+    }
 }
